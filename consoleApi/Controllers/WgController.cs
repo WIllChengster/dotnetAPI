9a66a7b652b01d3768823b37f0622c1b5c7ab7f6
burninggun <willchengsters@gmail.com>
2018-08-08 14:49:46 -0700

    Can use data from sql table for webgrabber

--- consoleApi/Controllers/WgController.cs
+++ consoleApi/Controllers/WgController.cs
@@ -22,22 +22,56 @@ namespace consoleApi
         public SqlConnection connection;
 
         //Get api/wg
+        [HttpGet]
         public IEnumerable<string> Get()
         {
+            Properties.Settings S = new Properties.Settings();
+            string DocName = "doc";
+            string HTML = "https://google.com";
+            using (connection = new SqlConnection(S.Database2ConnectionString))
+            {
 
+
+
+                connection.Open();
+
+                SqlCommand HTMLCommand = new SqlCommand("SELECT HTML FROM Settings WHERE Id = 1", connection);
+
+                SqlDataReader reader = HTMLCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    HTML = reader[0].ToString();
+                }
+                connection.Close();
+
+                connection.Open();
+
+                SqlCommand DocNameCommand = new SqlCommand("SELECT NewDocName FROM Settings WHERE Id = 1", connection);
+
+                reader = DocNameCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    DocName = reader[0].ToString();
+                }
+                Console.WriteLine(HTML + DocName);
+
+                connection.Close();
+            };
 
             APWebGrabber wg = new APWebGrabber();
 
             wg.OutputDirectory = @"C:/Users/Administrator/Desktop";
-            wg.NewDocumentName = "New Document.pdf";
-            wg.URL = "https://google.com";
+            wg.NewDocumentName = DocName;
+            wg.URL = HTML;
             wg.ConvertToPDF("127.0.0.1", 52525);
 
             return new string[] { "Hello", "World" };
         }
         
         
-
+            
         //Post
         [HttpPost]
         public void Post([FromBody] Setting opt)
@@ -57,11 +91,11 @@ namespace consoleApi
 
                 SqlCommand Command = new SqlCommand("UPDATE Settings SET NewDocName = @DocName WHERE Id = 1", connection);
 
-
                 SqlParameter param = new SqlParameter(
                     "@DocName", System.Data.SqlDbType.NVarChar, 16);
                 param.Value = opt.DocName;
                 Command.Parameters.Add(param);
+
                 Command.ExecuteNonQuery();
 
 
